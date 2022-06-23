5000febbd4b48d30bf8e83e38ab3e48ef507b327
ibm9471 <ibm.dev94@gmail.com>
2022-06-23 10:01:49 +0300

    remove setting from org admin and fix header

--- src/core/config/MainMenuConfig.ts
+++ src/core/config/MainMenuConfig.ts
@@ -82,38 +82,6 @@ const OrgAdminMenu = [
     svgIcon: "media/icons/duotune/general/gen019.svg",
     fontIcon: "bi-hospital",
   },
-  {
-    heading: "Settings",
-    route: "/settings",
-    svgIcon: "media/icons/duotune/general/gen019.svg",
-    fontIcon: "bi-gear",
-    pages: [
-      {
-        heading: "Health Fund List",
-        route: "/settings/health-fund",
-        svgIcon: "media/icons/duotune/general/gen022.svg",
-        fontIcon: "bi-archive",
-      },
-      {
-        heading: "Birthcode List",
-        route: "/settings/birthCode",
-        svgIcon: "media/icons/duotune/communication/com006.svg",
-        fontIcon: "bi-person",
-      },
-      {
-        heading: "Specialist Type",
-        route: "/settings/specialist-type",
-        svgIcon: "media/icons/duotune/technology/teh004.svg",
-        fontIcon: "bi-sticky",
-      },
-      {
-        heading: "Specialist Title",
-        route: "/settings/specialist-title",
-        svgIcon: "media/icons/duotune/art/art009.svg",
-        fontIcon: "bi-shield-check",
-      },
-    ],
-  },
 ];
 
 const OrgManagerMenu = [
