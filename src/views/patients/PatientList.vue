b4846a2b7e47297ecc9c5f6a9a74a3b3405b706f
IceDev528 <iceprince.L528@gmail.com>
2022-07-07 04:02:10 -0400

    Patient List update design

--- src/views/patients/PatientList.vue
+++ src/views/patients/PatientList.vue
@@ -1,48 +1,123 @@
 <template>
   <div class="card">
-    <div class="card-header border-0 pt-6">
-      <!--begin::Card title-->
-      <div class="card-title">
-        <!--begin::Search -->
-        <div class="d-flex align-items-center position-relative my-1">
-          <span class="svg-icon svg-icon-1 position-absolute ms-6">
+    <div class="card-toolbar border-0 p-5">
+      <!--begin::Compact form-->
+      <div class="d-flex align-items-center">
+        <!--begin::Input group-->
+        <div class="position-relative w-md-400px me-2">
+          <!--begin::Svg Icon | path: -->
+          <span
+            class="svg-icon svg-icon-3 svg-icon-gray-500 position-absolute top-50 translate-middle ms-6"
+          >
             <inline-svg src="media/icons/duotune/general/gen021.svg" />
           </span>
+          <!--end::Svg Icon-->
           <input
             type="text"
-            data-kt-subscription-table-filter="search"
-            class="form-control form-control-solid w-250px ps-14"
-            placeholder="Search Organisations"
+            class="form-control form-control-solid ps-10"
+            v-model="searchName"
+            placeholder="Search"
           />
         </div>
-        <!--end::Search-->
-      </div>
-      <!--begin::Card title-->
-
-      <!--begin::Card toolbar-->
-      <div class="card-toolbar">
-        <!--begin::Toolbar-->
-        <div
-          class="d-flex justify-content-end"
-          data-kt-subscription-table-toolbar="base"
+        <!--end::Input group-->
+        <!--begin:Action-->
+        <a
+          id="kt_horizontal_search_advanced_link"
+          class="btn btn-primary"
+          data-bs-toggle="collapse"
+          href="#kt_advanced_search_form"
+          >Advanced Search</a
         >
-          <!--begin::Export-->
-          <button
-            type="button"
-            class="btn btn-light-primary me-3"
-            data-bs-toggle="modal"
-            data-bs-target="#kt_subscriptions_export_modal"
-          >
-            <span class="svg-icon svg-icon-2">
-              <inline-svg src="media/icons/duotune/arrows/arr078.svg" />
-            </span>
-            Export
-          </button>
-          <!--end::Export-->
-        </div>
-        <!--end::Toolbar-->
+        <!--end:Action-->
       </div>
-      <!--end::Card toolbar-->
+      <!--end::Compact form-->
+      <!--begin::Advance form-->
+      <div class="collapse" id="kt_advanced_search_form">
+        <!--begin::Separator-->
+        <div class="separator separator-dashed mt-9 mb-6"></div>
+        <!--end::Separator-->
+        <el-form class="w-100" ref="formRef_1">
+          <!--begin::Row-->
+          <div class="row g-8">
+            <!--begin::Col-->
+            <div class="col-xxl-6">
+              <!--begin::Row-->
+              <div class="row g-8">
+                <!--begin::Col-->
+                <div class="col-lg-6">
+                  <label class="fs-6 form-label fw-bolder text-dark"
+                    >First Name</label
+                  >
+                  <el-form-item prop="first_name">
+                    <el-input type="text" />
+                  </el-form-item>
+                </div>
+                <!--end::Col-->
+                <!--begin::Col-->
+                <div class="col-lg-6">
+                  <label class="fs-6 form-label fw-bolder text-dark"
+                    >Last Name</label
+                  >
+                  <el-form-item prop="last_name">
+                    <el-input type="text" />
+                  </el-form-item>
+                </div>
+                <!--end::Col-->
+              </div>
+              <!--end::Row-->
+            </div>
+            <!--end::Col-->
+            <!--begin::Col-->
+            <div class="col-xxl-6">
+              <!--begin::Row-->
+              <div class="row g-8">
+                <!--begin::Col-->
+                <div class="col-lg-6">
+                  <label class="fs-6 form-label fw-bolder text-dark"
+                    >Date Of Birth</label
+                  >
+                  <el-form-item prop="date">
+                    <el-date-picker
+                      class="w-100"
+                      format="YYYY-MM"
+                      placeholder="2022-01-01"
+                    />
+                  </el-form-item>
+                </div>
+                <!--end::Col-->
+                <!--begin::Col-->
+                <div class="col-lg-6">
+                  <label class="fs-6 form-label fw-bolder text-dark"
+                    >UR Number</label
+                  >
+                  <el-form-item prop="last_name">
+                    <el-input type="text" />
+                  </el-form-item>
+
+                  <div
+                    class="d-flex align-items-center justify-content-end mt-5"
+                  >
+                    <button type="submit" class="btn btn-success me-5 w-25">
+                      Search
+                    </button>
+                    <button type="submit" class="btn btn-danger w-25">
+                      Reset
+                    </button>
+                  </div>
+                </div>
+                <!--end::Col-->
+              </div>
+              <!--end::Row-->
+            </div>
+            <!--end::Col-->
+          </div>
+          <!--end::Row-->
+          <!--begin::Separator-->
+        </el-form>
+        <div class="separator separator-dashed mt-9 mb-6"></div>
+        <!--end::Separator-->
+      </div>
+      <!--end::Advance form-->
     </div>
     <div class="card-body pt-0">
       <Datatable
@@ -52,23 +127,19 @@
         :loading="loading"
         :enable-items-per-page-dropdown="false"
       >
-        <template v-slot:cell-logo="{ row: item }">
-          <div class="symbol symbol-45px me-2">
-            <span class="symbol-label">
-              <img :src="item.logo" class="h-50 align-self-center" />
-            </span>
-          </div>
+        <template v-slot:cell-UR_number="{ row: item }">
+          {{ item.UR_number }}
         </template>
         <template v-slot:cell-full_name="{ row: item }">
           <span class="text-dark fw-bolder text-hover-primary mb-1 fs-6">
             {{ item.first_name }} {{ item.last_name }}
           </span>
-          <span class="text-muted fw-bold d-block">{{
-            item.phone_number
-          }}</span>
         </template>
-        <template v-slot:cell-email="{ row: item }">
-          {{ item.email }}
+        <template v-slot:cell-mobile_number="{ row: item }">
+          {{ item.mobile_number }}
+        </template>
+        <template v-slot:cell-upcoming="{ row: item }">
+          {{ item.upcoming_appointment }}
         </template>
         <template v-slot:cell-action="{ row: item }">
           <button
@@ -86,7 +157,14 @@
 </template>
 
 <script>
-import { defineComponent, onMounted, ref, computed, watchEffect } from "vue";
+import {
+  defineComponent,
+  onMounted,
+  ref,
+  watch,
+  computed,
+  watchEffect,
+} from "vue";
 import { useStore } from "vuex";
 import { useRouter } from "vue-router";
 import { setCurrentPageBreadcrumbs } from "@/core/helpers/breadcrumb";
@@ -105,8 +183,8 @@ export default defineComponent({
     const router = useRouter();
     const tableHeader = ref([
       {
-        name: "Logo",
-        key: "logo",
+        name: "UR Number",
+        key: "UR_number",
         sortable: true,
         searchable: true,
       },
@@ -117,10 +195,14 @@ export default defineComponent({
         searchable: true,
       },
       {
-        name: "Email",
-        key: "email",
+        name: "Number",
+        key: "mobile_number",
         sortable: true,
         searchable: true,
+      },
+      {
+        name: "Upcoming Appointments",
+        key: "upcoming",
       },
       {
         name: "Action",
@@ -130,14 +212,27 @@ export default defineComponent({
     const tableData = ref([]);
     const list = computed(() => store.getters.patientsList);
     const loading = ref(true);
+    const searchName = ref("");
 
     const handleView = (item) => {
       store.commit(Mutations.SET_PATIENT.SELECT, item);
       router.push({ name: "patients-view-appointments" });
     };
 
+    watch(searchName, () => {
+      console.log(searchName.value);
+      // tableData.value = tableData.value.filter(
+      //   (data) =>
+      //     data.first_name
+      //       .toLowerCase()
+      //       .includes(searchName.value.toLowerCase()) ||
+      //     data.last_name.toLowerCase().includes(searchName.value.toLowerCase())
+      // );
+    });
+
     watchEffect(() => {
       tableData.value = list;
+      console.log(tableData.value);
     });
 
     onMounted(() => {
@@ -149,7 +244,7 @@ export default defineComponent({
       });
     });
 
-    return { tableHeader, tableData, handleView };
+    return { tableHeader, tableData, searchName, handleView };
   },
 });
 </script>
