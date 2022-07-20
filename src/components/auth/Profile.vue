0a3802d49c8d50562ec6524afe63bf128ae059fd
IceDev528 <iceprince.L528@gmail.com>
2022-07-20 10:53:22 -0400

    Profile page update
    Co-authored-by: Paul <paul.ibm9471@gmail.com>

---
+++ src/components/auth/Profile.vue
@@ -0,0 +1,34 @@
+<template>
+  <!--begin::Navbar-->
+  <div class="card h-100">
+    <div class="card-body pt-9 pb-0">
+      <!--begin::Details-->
+      <el-form
+        @submit.prevent="submit()"
+        :model="formData"
+        :rules="rules"
+        ref="formRef"
+      >
+        <el-form-item prop="first_name">
+          <el-input
+            type="text"
+            class="w-50"
+            v-model="formData.first_name"
+            placeholder="First Name"
+          />
+        </el-form-item>
+      </el-form>
+      <!--end::Details-->
+    </div>
+  </div>
+  <!--end::Navbar-->
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "profile-page-layout",
+  components: {},
+});
+</script>
