d4421147e5dafe4d3bdfe2629bf3059bcade3d54
Kaylee Herrmann <106498280+kgherrmann@users.noreply.github.com>
2022-08-04 15:13:54 +1000

    Filling in menu buttons

---
+++ src/views/booking/EmployeeBookings.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class=""></div>
+</template>
+<script>
+import {
+  defineComponent,
+  ref,
+  watch,
+  reactive,
+  onMounted,
+  computed,
+} from "vue";
+import { useStore } from "vuex";
+import { Actions, Mutations } from "@/store/enums/StoreEnums";
+import { Modal } from "bootstrap";
+import { setCurrentPageBreadcrumbs } from "@/core/helpers/breadcrumb";
+
+export default defineComponent({
+  name: "employee-bookings-dashboard",
+  components: {},
+  setup() {
+    onMounted(() => {
+      setCurrentPageBreadcrumbs("My Bookings", ["Booking Dashboard"]);
+    });
+  },
+});
+</script>
