bc4d35e986a725999ab29fde4b788fa2182959a2
Kaylee Herrmann <106498280+kgherrmann@users.noreply.github.com>
2022-08-05 12:41:46 +1000

    index

---
+++ src/views/settings/PreAdmissionConsent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { defineComponent, onMounted } from "vue";
+
+import { setCurrentPageBreadcrumbs } from "@/core/helpers/breadcrumb";
+
+export default defineComponent({
+  name: "pre-admission-consent",
+
+  components: {},
+
+  setup() {
+    onMounted(() => {
+      setCurrentPageBreadcrumbs("Pre-Admission Consent", ["Settings"]);
+    });
+  },
+});
+</script>
