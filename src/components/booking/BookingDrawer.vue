c560f69cc9a6316ed0ad591a16e390bdafd03b62
Kaylee Herrmann <106498280+kgherrmann@users.noreply.github.com>
2022-08-13 16:10:46 +1000

    Fix override issue

--- src/components/booking/BookingDrawer.vue
+++ src/components/booking/BookingDrawer.vue
@@ -169,7 +169,6 @@
           />
           <!--Move Appointment-->
           <BookingDrawerButton
-            @click="handleEdit"
             :heading="'Move'"
             :subheading="'Appointment'"
             :iconPath="'media/icons/duotune/arrows/arr035.svg'"
