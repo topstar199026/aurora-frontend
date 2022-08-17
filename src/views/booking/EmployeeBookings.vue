57f2e50bccea624ea2b60ec8cd9e2a76ddf2da18
Paul <ibm.dev94@gmail.com>
2022-08-18 06:43:03 +0900

    Specialist Booking Dashboard
    - Add the full calendar to th Specialist Booking Dashboard

--- src/views/booking/EmployeeBookings.vue
+++ src/views/booking/EmployeeBookings.vue
@@ -1,13 +1,19 @@
 <template>
-  <div class=""></div>
+  <div class="">
+    <AptCalendar></AptCalendar>
+  </div>
 </template>
+
 <script>
 import { defineComponent, onMounted } from "vue";
 import { setCurrentPageBreadcrumbs } from "@/core/helpers/breadcrumb";
+import AptCalendar from "@/components/booking/AptCalendar.vue";
 
 export default defineComponent({
   name: "employee-bookings-dashboard",
-  components: {},
+  components: {
+    AptCalendar,
+  },
   setup() {
     onMounted(() => {
       setCurrentPageBreadcrumbs("My Bookings", ["Booking Dashboard"]);
