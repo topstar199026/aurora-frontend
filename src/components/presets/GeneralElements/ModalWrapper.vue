3c4954f8adac7b9c9ef19897e4c834843ce11977
Kaylee Herrmann <kaylee@ojc.com.au>
2022-09-07 10:40:43 +1000

    Update Procedure approval modal

--- src/components/presets/GeneralElements/ModalWrapper.vue
+++ src/components/presets/GeneralElements/ModalWrapper.vue
@@ -8,16 +8,22 @@
   >
     <div class="modal-dialog modal-dialog-centered mw-650px">
       <div class="modal-content">
-        <div class="modal-header" :id="'kt_modal_' + modalId + '_header'">
-          <h2 class="fw-bolder">{{ title }}</h2>
-          <div
-            :id="'kt_modal_' + modalId + '_close'"
-            data-bs-dismiss="modal"
-            class="btn btn-icon btn-sm btn-active-icon-primary"
-          >
-            <span class="svg-icon svg-icon-1">
-              <inline-svg src="media/icons/duotune/arrows/arr061.svg" />
-            </span>
+        <div
+          class="modal-header d-flex flex-row"
+          :id="'kt_modal_' + modalId + '_header'"
+        >
+          <HeadingText :text="title" />
+          <div class="d-flex flex-row justify-content-end">
+            <slot name="header-actions"></slot>
+            <div
+              :id="'kt_modal_' + modalId + '_close'"
+              data-bs-dismiss="modal"
+              class="btn btn-icon btn-sm btn-active-icon-primary"
+            >
+              <span class="svg-icon svg-icon-1">
+                <inline-svg src="media/icons/duotune/arrows/arr061.svg" />
+              </span>
+            </div>
           </div>
         </div>
         <div class="modal-body">
