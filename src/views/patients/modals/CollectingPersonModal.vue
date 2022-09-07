a5570036340302baf2b61833af78de3eada348cc
Kaylee Herrmann <kaylee@ojc.com.au>
2022-09-07 11:03:01 +1000

    Clean Collecting person modal

--- src/views/patients/modals/CollectingPersonModal.vue
+++ src/views/patients/modals/CollectingPersonModal.vue
@@ -1,150 +1,57 @@
 <template>
-  <div
-    class="modal fade"
-    id="modal_collecting_person"
-    tabindex="-1"
-    aria-hidden="true"
-    ref="collectingPersonModalRef"
+  <ModalWrapper
+    title="Update Collecting Person"
+    modalId="collecting_person"
+    modalRef="collectingPersonModalRef"
   >
-    <!--begin::Modal dialog-->
-    <div class="modal-dialog modal-dialog-centered mw-650px">
-      <!--begin::Modal content-->
-      <div class="modal-content">
-        <!--begin::Modal header-->
-        <div class="modal-header" id="kt_modal_collecting_person_header">
-          <!--begin::Modal title-->
-          <h2 class="fw-bolder">Update Collecting Person</h2>
-          <!--end::Modal title-->
-
-          <!--begin::Close-->
-          <div
-            id="kt_modal_collecting_person_close"
-            data-bs-dismiss="modal"
-            class="btn btn-icon btn-sm btn-active-icon-primary"
-          >
-            <span class="svg-icon svg-icon-1">
-              <inline-svg src="media/icons/duotune/arrows/arr061.svg" />
-            </span>
-          </div>
-          <!--end::Close-->
-        </div>
-        <!--end::Modal header-->
-        <!--begin::Form-->
-        <el-form
-          @submit.prevent="submit()"
-          :model="formData"
-          :rules="rules"
-          ref="formRef"
+    <el-form
+      @submit.prevent="submit()"
+      :model="formData"
+      :rules="rules"
+      ref="formRef"
+    >
+      <InputWrapper label="Name" prop="collecting_person_name">
+        <el-input
+          v-model="formData.collecting_person_name"
+          type="text"
+          placeholder="Enter Name"
+        />
+      </InputWrapper>
+      <InputWrapper label="Phone" prop="collecting_person_phone">
+        <el-input
+          type="text"
+          v-mask="'0#-####-####'"
+          v-model="formData.collecting_person_phone"
+          placeholder="Enter Phone"
+        />
+      </InputWrapper>
+      <InputWrapper
+        label="Alternate Contact"
+        prop="collecting_person_alternate_contact"
+      >
+        <el-input
+          v-model="formData.collecting_person_alternate_contact"
+          type="text"
+          placeholder="Enter Alternate Contact"
+        />
+      </InputWrapper>
+      <div class="modal-footer flex-center">
+        <button
+          :data-kt-indicator="loading ? 'on' : null"
+          class="btn btn-lg btn-primary m-6"
+          type="submit"
         >
-          <!--begin::Modal body-->
-          <div class="modal-body py-10 px-lg-17">
-            <!--begin::Scroll-->
-            <div
-              class="scroll-y me-n7 pe-7"
-              id="kt_modal_collecting_person_scroll"
-              data-kt-scroll="true"
-              data-kt-scroll-activate="{default: false, lg: true}"
-              data-kt-scroll-max-height="auto"
-              data-kt-scroll-dependencies="#kt_modal_collecting_person_header"
-              data-kt-scroll-wrappers="#kt_modal_collecting_person_scroll"
-              data-kt-scroll-offset="300px"
-            >
-              <!--begin::Input group-->
-              <div class="fv-row mb-7">
-                <!--begin::Label-->
-                <label class="required fs-6 fw-bold mb-2">Name</label>
-                <!--end::Label-->
-
-                <!--begin::Input-->
-                <el-form-item prop="collecting_person_name">
-                  <el-input
-                    v-model="formData.collecting_person_name"
-                    type="text"
-                    placeholder="Enter Name"
-                  />
-                </el-form-item>
-                <!--end::Input-->
-              </div>
-              <!--end::Input group-->
-
-              <!--begin::Input group-->
-              <div class="fv-row mb-7">
-                <!--begin::Label-->
-                <label class="required fs-6 fw-bold mb-2">Phone</label>
-                <!--end::Label-->
-
-                <!--begin::Input-->
-                <el-form-item prop="collecting_person_phone">
-                  <el-input
-                    type="text"
-                    v-mask="'0#-####-####'"
-                    v-model="formData.collecting_person_phone"
-                    placeholder="Enter Phone"
-                  />
-                </el-form-item>
-                <!--end::Input-->
-              </div>
-              <!--end::Input group-->
-
-              <!--begin::Input group-->
-              <div class="fv-row mb-7">
-                <!--begin::Label-->
-                <label class="required fs-6 fw-bold mb-2"
-                  >Alternate Contact</label
-                >
-                <!--end::Label-->
-
-                <!--begin::Input-->
-                <el-form-item prop="collecting_person_alternate_contact">
-                  <el-input
-                    v-model="formData.collecting_person_alternate_contact"
-                    type="text"
-                    placeholder="Enter Alternate Contact"
-                  />
-                </el-form-item>
-                <!--end::Input-->
-              </div>
-              <!--end::Input group-->
-            </div>
-            <!--end::Scroll-->
-          </div>
-          <!--end::Modal body-->
-
-          <!--begin::Modal footer-->
-          <div class="modal-footer flex-center">
-            <!--begin::Button-->
-            <button
-              type="reset"
-              data-bs-dismiss="modal"
-              id="kt_modal_collecting_person_cancel"
-              class="btn btn-light me-3"
-            >
-              Cancel
-            </button>
-            <!--end::Button-->
-
-            <!--begin::Button-->
-            <button
-              :data-kt-indicator="loading ? 'on' : null"
-              class="btn btn-lg btn-primary"
-              type="submit"
-            >
-              <span v-if="!loading" class="indicator-label"> Update </span>
-              <span v-if="loading" class="indicator-progress">
-                Please wait...
-                <span
-                  class="spinner-border spinner-border-sm align-middle ms-2"
-                ></span>
-              </span>
-            </button>
-            <!--end::Button-->
-          </div>
-          <!--end::Modal footer-->
-        </el-form>
-        <!--end::Form-->
+          <span v-if="!loading" class="indicator-label"> Update </span>
+          <span v-if="loading" class="indicator-progress">
+            Please wait...
+            <span
+              class="spinner-border spinner-border-sm align-middle ms-2"
+            ></span>
+          </span>
+        </button>
       </div>
-    </div>
-  </div>
+    </el-form>
+  </ModalWrapper>
 </template>
 
 <script>
