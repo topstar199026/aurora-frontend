2ff1624b708750a78b70e8eaa704344da655b046
topstar199026 <topstar199026@gmail.com>
2022-09-20 04:54:09 +0900

    PM-1: work version

--- src/views/patients/PatientList.vue
+++ src/views/patients/PatientList.vue
@@ -247,7 +247,7 @@ export default defineComponent({
           renderTable();
         });
     };
-
+    searchPatient();
     const clearFilters = () => {
       filter.first_name = "";
       filter.last_name = "";
