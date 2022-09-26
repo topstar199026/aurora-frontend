14813da893c4c2e264e2c0b0a011af75f8b8feea
horita5282 <horitdaisuke5282@gmail.com>
2022-09-26 18:27:05 +0900

    work-06 add provider no field and search bar

--- src/views/settings/referring-doctors/RefDoctors.vue
+++ src/views/settings/referring-doctors/RefDoctors.vue
@@ -1,9 +1,22 @@
 <template>
   <div class="card w-75 mx-auto">
-    <div class="card-header row border-0 p-6">
-      <div class="card-title col"></div>
+    <div class="card-header border-0 pt-6">
+      <div class="card-title">
+        <div class="d-flex align-items-center position-relative my-1">
+          <span class="svg-icon svg-icon-1 position-absolute ms-6">
+            <inline-svg src="media/icons/duotune/general/gen021.svg" />
+          </span>
+          <input
+            type="text"
+            data-kt-subscription-table-filter="search"
+            class="form-control form-control-solid w-250px ps-14"
+            placeholder="Search Referring Doctors"
+            v-model="filterAndSort.searchText"
+          />
+        </div>
+      </div>
       <!--begin::Add button-->
-      <div class="card-toolbar col-12">
+      <div class="card-toolbar">
         <router-link
           to="/settings/referring-doctors/create"
           type="button"
@@ -22,8 +35,12 @@
         :table-header="tableHeader"
         :table-data="tableData"
         :rows-per-page="10"
+        :loading="loading"
         :enable-items-per-page-dropdown="true"
       >
+        <template v-slot:cell-provider_no="{ row: item }">
+          {{ item.provider_no }}
+        </template>
         <template v-slot:cell-full_name="{ row: item }">
           {{ item.full_name }}
         </template>
@@ -58,7 +75,15 @@
 </template>
 
 <script>
-import { defineComponent, onMounted, ref, computed, watchEffect } from "vue";
+import {
+  defineComponent,
+  onMounted,
+  ref,
+  computed,
+  watchEffect,
+  reactive,
+  watch,
+} from "vue";
 import { useStore } from "vuex";
 import { useRouter } from "vue-router";
 import { setCurrentPageBreadcrumbs } from "@/core/helpers/breadcrumb";
@@ -76,7 +101,15 @@ export default defineComponent({
   setup() {
     const store = useStore();
     const router = useRouter();
+    const filterAndSort = reactive({
+      searchText: "",
+    });
     const tableHeader = ref([
+      {
+        name: "Provider Number",
+        key: "provider_no",
+        sortable: true,
+      },
       {
         name: "Full Name",
         key: "full_name",
@@ -98,6 +131,8 @@ export default defineComponent({
       },
     ]);
     const tableData = ref([]);
+    const filteredData = ref([]);
+    const loading = ref(true);
     const referringDoctors = computed(
       () => store.getters.getReferralDoctorList
     );
@@ -132,9 +167,45 @@ export default defineComponent({
 
     watchEffect(() => {
       tableData.value = referringDoctors;
+      loading.value = false;
     });
 
-    return { tableHeader, tableData, handleEdit, handleDelete };
+    const applyFilterAndSort = () => {
+      if (filterAndSort.searchText != "") {
+        filteredData.value = referringDoctors.value.filter((org) => {
+          if (
+            org.provider_no
+              .toLowerCase()
+              .search(filterAndSort.searchText.toLowerCase()) >= 0
+          ) {
+            return true;
+          }
+
+          if (
+            org.full_name
+              .toLowerCase()
+              .search(filterAndSort.searchText.toLowerCase()) >= 0
+          ) {
+            return true;
+          }
+
+          return false;
+        });
+      } else {
+        filteredData.value = referringDoctors.value;
+      }
+      tableData.value = filteredData;
+    };
+
+    watchEffect(() => {
+      applyFilterAndSort();
+    });
+
+    watch(filterAndSort, () => {
+      applyFilterAndSort();
+    });
+
+    return { tableHeader, tableData, handleEdit, handleDelete, filterAndSort };
   },
 });
 </script>
