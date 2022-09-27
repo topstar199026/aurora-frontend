02c5ad47c0b1363cf7c1a47a77d19573ad2f9e62
Kaylee Herrmann <kaylee@ojc.com.au>
2022-09-27 10:43:13 +1000

    Patch: Linting

--- src/router/SettingsRoutes.ts
+++ src/router/SettingsRoutes.ts
@@ -66,13 +66,12 @@ const SettingsRoutes = [
       import("@/views/settings/anesthetic-questions/AnestheticQuestions.vue"),
   },
   {
-
     path: "/settings/letter-templates",
     name: "letterTypes",
     component: () =>
       import("@/views/settings/letter-templates/LetterTemplates.vue"),
-},
-{
+  },
+  {
     path: "/settings/referring-doctors",
     name: "refDoctors",
     component: () =>
@@ -89,7 +88,6 @@ const SettingsRoutes = [
     name: "editRefDoctors",
     component: () =>
       import("@/views/settings/referring-doctors/EditRefDoctors.vue"),
-
   },
 ];
 
