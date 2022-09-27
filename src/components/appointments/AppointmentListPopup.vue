048a93865b2abffec493aa11c3ef7677bbd20948
topstar199026 <topstar199026@gmail.com>
2022-09-27 23:29:54 +0900

    BM-15: work version

--- src/components/appointments/AppointmentListPopup.vue
+++ src/components/appointments/AppointmentListPopup.vue
@@ -5,7 +5,24 @@
     modalRef="AppointmentListPopupModalRef"
   >
     <div class="d-flex flex-row">
-      <div></div>
+      <div
+        class="d-flex align-items-center justify-content-center cursor-pointer"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-chevron-compact-left"
+          viewBox="0 0 16 16"
+          style="width: 32px; height: 32px"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M9.224 1.553a.5.5 0 0 1 .223.67L6.56 8l2.888 5.776a.5.5 0 1 1-.894.448l-3-6a.5.5 0 0 1 0-.448l3-6a.5.5 0 0 1 .67-.223z"
+          />
+        </svg>
+      </div>
       <div class="flex">
         <div class="search-params d-flex flex-wrap gap-4">
           <h4 class="text-nowrap" style="color: var(--el-color-info)">
@@ -89,7 +106,24 @@
           <p v-else>No Next available Appointments.</p>
         </div>
       </div>
-      <div></div>
+      <div
+        class="d-flex align-items-center justify-content-center cursor-pointer"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-chevron-compact-right"
+          viewBox="0 0 16 16"
+          style="width: 32px; height: 32px"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M6.776 1.553a.5.5 0 0 1 .671.223l3 6a.5.5 0 0 1 0 .448l-3 6a.5.5 0 1 1-.894-.448L9.44 8 6.553 2.224a.5.5 0 0 1 .223-.671z"
+          />
+        </svg>
+      </div>
     </div>
   </ModalWrapper>
 </template>
