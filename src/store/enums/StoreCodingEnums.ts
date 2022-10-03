743d8292b1e21d0f8c1a672bd2d3fda2e4e2e2cb
Kaylee Herrmann <kaylee@ojc.com.au>
2022-10-04 10:52:08 +1100

    Init pages

---
+++ src/store/enums/StoreCodingEnums.ts
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+const CodingActions = {
+    SEARCH_DIAGNOSES: "searchDiagnosisCodes",
+};
+
+const CodingMutations = {
+  
+};
+
+export { CodingActions, CodingMutations };
---
+++ src/views/coding/dashboard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="card w-75 mx-auto">
+    <h1 class="m-auto p-6">CODING</h1>
+  </div>
+</template>
+
+<script>
+import { defineComponent, onMounted } from "vue";
+import { setCurrentPageBreadcrumbs } from "@/core/helpers/breadcrumb";
+import { CodingActions, CodingMutations } from "@/store/enums/StoreCodingEnums";
+import { useStore } from "vuex";
+export default defineComponent({
+  name: "coding-dashboard",
+  setup() {
+    const store = useStore();
+    onMounted(() => {
+      setCurrentPageBreadcrumbs("Coding");
+      store
+        .dispatch(CodingActions.SEARCH_DIAGNOSES, "cov")
+        .then((response) => {
+          console.log(response.data[3]);
+        })
+        .catch((response) => {
+          console.log(response);
+        });
+    });
+  },
+});
+</script>
