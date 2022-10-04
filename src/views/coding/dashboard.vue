f299afccb23e90d2093c9ae86be56c253ac30f93
Kaylee Herrmann <kaylee@ojc.com.au>
2022-10-05 07:32:11 +1100

    Replace search

--- src/views/coding/dashboard.vue
+++ src/views/coding/dashboard.vue
@@ -1,29 +1,9 @@
-<template>
-  <div class="card w-75 mx-auto">
-    <h1 class="m-auto p-6">CODING</h1>
-  </div>
-</template>
+<template>Coding Dashboard</template>
 
 <script>
-import { defineComponent, onMounted } from "vue";
-import { setCurrentPageBreadcrumbs } from "@/core/helpers/breadcrumb";
-import { CodingActions, CodingMutations } from "@/store/enums/StoreCodingEnums";
-import { useStore } from "vuex";
+import { defineComponent } from "vue";
+
 export default defineComponent({
   name: "coding-dashboard",
-  setup() {
-    const store = useStore();
-    onMounted(() => {
-      setCurrentPageBreadcrumbs("Coding");
-      store
-        .dispatch(CodingActions.SEARCH_DIAGNOSES, "cov")
-        .then((response) => {
-          console.log(response.data[3]);
-        })
-        .catch((response) => {
-          console.log(response);
-        });
-    });
-  },
 });
 </script>
