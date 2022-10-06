39b18c27739b9b6ce07cbab2662af3e0a310ffe1
Kaylee Herrmann <kaylee@ojc.com.au>
2022-10-06 15:55:35 +1100

    Add edit/delete buttons to Patient allergies

--- src/components/presets/PatientElements/PatientAllergiesTable.vue
+++ src/components/presets/PatientElements/PatientAllergiesTable.vue
@@ -4,19 +4,19 @@
       <th>Name</th>
       <th>Severity</th>
       <th>Symptoms</th>
+      <th></th>
     </tr>
-
-    <tr v-for="allergy in patient.allergies" :key="allergy.id">
-      <td>{{ allergy.name }}</td>
-      <td>{{ allergy.severity }}</td>
-      <td>{{ allergy.symptoms }}</td>
-    </tr>
+    <template v-for="allergy in patient.allergies" :key="allergy.id">
+      <PatientAllergiesRow :allergy="allergy" />
+    </template>
   </table>
 </template>
 <script lang="ts">
+import PatientAllergiesRow from "./PatientAllergiesRow.vue";
 export default {
   props: {
     patient: { required: true, type: Object },
   },
+  components: { PatientAllergiesRow },
 };
 </script>
