6606da05fd3ed5622aa1f7bdba78d0ee3f904971
topstar199026 <topstar199026@gmail.com>
2022-10-17 09:51:30 +0900

    BM-29: add feature to confirm

--- src/views/appointments/UnconfirmedApts.vue
+++ src/views/appointments/UnconfirmedApts.vue
@@ -13,18 +13,37 @@
 
 <script>
 import { defineComponent, onMounted } from "vue";
+import { useStore } from "vuex";
 import { setCurrentPageBreadcrumbs } from "@/core/helpers/breadcrumb";
 import AppointmentList from "@/views/appointments/AppointmentList.vue";
+import {
+  AppointmentActions,
+  AppointmentMutations,
+} from "@/store/enums/StoreAppointmentEnums";
+
 export default defineComponent({
   name: "admin-main",
   components: { AppointmentList },
   setup() {
+    const store = useStore();
     onMounted(() => {
       setCurrentPageBreadcrumbs("Unconfirmed Appointments", ["Booking"]);
     });
 
-    const handleConfirmAppointment = (appointmentId) => {
+    const handleConfirmAppointment = async (appointmentId) => {
       console.log(appointmentId);
+      await store
+        .dispatch(AppointmentActions.CONFIRMATION_STATUS.UPDATE, {
+          id: appointmentId,
+          confirmed: true,
+        })
+        .then(() => {
+          // store.dispatch(
+          //   AppointmentActions.BOOKING.SEARCH.SPECIALISTS,
+          //   searchVal.value
+          // );
+          // DrawerComponent?.getInstance("appointment-drawer")?.hide();
+        });
     };
 
     const handleCancelAppointment = (appointmentId) => {
