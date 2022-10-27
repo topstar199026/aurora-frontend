5d79fd513b0b1dd7ce5b28cd29edbc00dddb8e5a
topstar199026 <topstar199026@gmail.com>
2022-10-27 18:17:57 +0900

    COM-34: fix patient detail issue

--- src/core/data/genders.ts
+++ src/core/data/genders.ts
@@ -1,8 +1,8 @@
 export const genders = [
-  { value: 1, label: "Male" },
-  { value: 2, label: "Female" },
-  { value: 3, label: "Other" },
-  { value: 9, label: "Undisclosed" },
+  { value: "1", label: "Male" },
+  { value: "2", label: "Female" },
+  { value: "3", label: "Other" },
+  { value: "9", label: "Undisclosed" },
 ];
 
 export default genders;
