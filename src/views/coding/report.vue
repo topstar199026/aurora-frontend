970b1df92c3a3932f14648a3a3054db402e3a728
horita5282 <horitdaisuke5282@gmail.com>
2022-11-24 11:58:47 +0900

    c-06:work-02 created generating coding page

--- src/views/coding/report.vue
+++ src/views/coding/report.vue
@@ -1,33 +1,75 @@
 <template>
-  <CardSection>
-    <div class="d-flex flex-row-reverse">
-      <div class="form-check form-switch form-check-custom form-check-solid">
-        <input
-          class="form-check-input"
-          type="checkbox"
-          value=""
-          id="flexSwitchChecked"
-          checked="checked"
-        />
-        <label class="form-check-label" for="flexSwitchChecked">
-          Show Complete
-        </label>
-      </div>
+  <div class="card pb-9">
+    <ProfileNavigation />
+    <div class="card-body pt-9 pb-0">
+      <!--begin::Details-->
+      <el-form
+        @submit.prevent="submit()"
+        :model="formData"
+        :rules="rules"
+        ref="formRef"
+      >
+        <div class="row">
+          <InputWrapper
+            class="col-6"
+            required
+            prop="from_date"
+            label="From Date"
+          >
+            <el-form-item>
+              <el-date-picker
+                class="w-100"
+                v-model="formData.from_date"
+                format="DD/MM/YYYY"
+                placeholder="01/01/1990"
+              />
+            </el-form-item>
+          </InputWrapper>
+          <InputWrapper class="col-6" required prop="to_date" label="From Date">
+            <el-form-item>
+              <el-date-picker
+                class="w-100"
+                v-model="formData.to_date"
+                format="DD/MM/YYYY"
+                placeholder="01/01/1990"
+              />
+            </el-form-item>
+          </InputWrapper>
+          <InputWrapper class="col-6" required prop="type" label="Report Type">
+            <el-form-item>
+              <el-select
+                class="w-100"
+                placeholder="Select Time frame"
+                v-model="formData.type"
+              >
+                <el-option
+                  v-for="item in codingReportTypes"
+                  :value="item"
+                  :label="item"
+                  :key="item"
+                />
+              </el-select>
+            </el-form-item>
+          </InputWrapper>
+        </div>
+        <div class="d-flex ms-auto justify-content-end w-50">
+          <button type="submit" class="btn btn-primary w-100 w-sm-25">
+            Save
+          </button>
+          <!--begin::Button-->
+          <router-link
+            type="reset"
+            to="/booking/dashboard"
+            class="btn btn-light-primary w-100 w-sm-25 ms-2"
+          >
+            Cancel
+          </router-link>
+          <!--end::Button-->
+        </div>
+      </el-form>
+      <!--end::Details-->
     </div>
-
-    <Datatable
-      :table-header="tableHeader"
-      :table-data="tableData"
-      :rows-per-page="20"
-      :loading="loading"
-      :enable-items-per-page-dropdown="true"
-    >
-      <template v-slot:cell-complete="{ row: item }">
-        {{ item.codes.is_complete }}
-      </template>
-    </Datatable>
-    <CodingModal></CodingModal>
-  </CardSection>
+  </div>
 </template>
 
 <script>
@@ -35,44 +77,53 @@ import { defineComponent, onMounted, ref, computed, watchEffect } from "vue";
 import { useStore } from "vuex";
 import { setCurrentPageBreadcrumbs } from "@/core/helpers/breadcrumb";
 import { CodingActions, CodingMutations } from "@/store/enums/StoreCodingEnums";
-import Datatable from "@/components/kt-datatable/KTDatatable.vue";
-import CodingModal from "@/views/coding/codingModal.vue";
-import { Modal } from "bootstrap";
+import { codingReportTypes } from "@/core/data/coding-report-types";
 
 export default defineComponent({
   name: "coding-reports",
-  components: {
-    Datatable,
-    CodingModal,
-  },
   setup() {
     const store = useStore();
-
-    const tableData = ref([]);
-    const aptList = computed(() => store.getters.getCodingAptList);
     const loading = ref(true);
-    const tableHeader = ref([
-      {
-        name: "Appointment",
-        key: "appointment",
-        sortable: true,
-      },
-    ]);
-
-    onMounted(() => {
-      setCurrentPageBreadcrumbs("Coding");
-      store.dispatch(CodingActions.LIST);
+    const formRef = ref(null);
+    const formData = ref({
+      from_date: null,
+      to_date: null,
+      type: null,
     });
 
-    watchEffect(() => {
-      tableData.value = aptList;
-      loading.value = false;
+    const rules = ref({
+      from_date: [
+        {
+          required: true,
+          message: "From date cannot be blank",
+          trigger: "change",
+        },
+      ],
+      to_date: [
+        {
+          required: true,
+          message: "To date cannot be blank",
+          trigger: "change",
+        },
+      ],
+      type: [
+        {
+          required: true,
+          message: "Type cannot be blank",
+          trigger: "change",
+        },
+      ],
+    });
+
+    onMounted(() => {
+      setCurrentPageBreadcrumbs("Coding Reports");
     });
 
     return {
-      tableData,
-      tableHeader,
       loading,
+      formData,
+      rules,
+      codingReportTypes,
     };
   },
 });
