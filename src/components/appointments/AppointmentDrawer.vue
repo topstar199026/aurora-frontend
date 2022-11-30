8ff0f91c728409955d508e84706acf47e9382c63
Kaylee Herrmann <kaylee@ojc.com.au>
2022-11-30 12:10:50 +1100

    PATCH: Lint error from conflict resolve

--- src/components/appointments/AppointmentDrawer.vue
+++ src/components/appointments/AppointmentDrawer.vue
@@ -116,7 +116,6 @@
             <label class="fs-5 text-danger"
               >Allergies:
               <span v-if="patient.allergies && patient.allergies.length > 0">
-
                 <template
                   v-for="allergy in patient.allergies"
                   :key="allergy.id"
