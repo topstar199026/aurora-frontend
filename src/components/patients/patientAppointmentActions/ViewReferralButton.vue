a43840b946db89e8900a951da1c048b3f6c5e967
topstar199026 <topstar199026@gmail.com>
2022-12-12 20:32:56 +0900

    PM-41: fix multi referral issue

--- src/components/patients/patientAppointmentActions/ViewReferralButton.vue
+++ src/components/patients/patientAppointmentActions/ViewReferralButton.vue
@@ -1,16 +1,11 @@
 <template>
   <LargeIconButton text="View Referral" @click="handleReferral()" />
-  <AppointmentReferralModal
-    :appointment="appointment"
-  ></AppointmentReferralModal>
 </template>
 
 <script lang="ts">
 import { Modal } from "bootstrap";
-import AppointmentReferralModal from "@/views/patients/modals/AppointmentReferralModal.vue";
 
 export default {
-  components: { AppointmentReferralModal },
   props: {
     appointment: {
       required: true,
@@ -19,10 +14,12 @@ export default {
   },
   setup() {
     const handleReferral = () => {
-      const modal = new Modal(
-        document.getElementById("modal_appointment_referral")
-      );
-      modal.show();
+      setTimeout(() => {
+        const modal = new Modal(
+          document.getElementById("modal_appointment_referral")
+        );
+        modal.show();
+      }, 100);
     };
 
     return { handleReferral };
--- src/views/patients/Appointments.vue
+++ src/views/patients/Appointments.vue
@@ -56,7 +56,10 @@
             </div>
           </div>
           <div v-else class="d-flex flex-column">No referral information</div>
-          <ViewReferralButton :appointment="item" />
+          <ViewReferralButton
+            :appointment="item"
+            @click="handleReferralClick(item)"
+          />
         </template>
         <template v-slot:cell-attendance_status="{ row: item }">
           <div class="d-flex flex-column">
@@ -98,6 +101,10 @@
       </Datatable>
     </template>
   </CardSection>
+  <AppointmentReferralModal
+    v-if="referralAppointment"
+    :appointment="referralAppointment"
+  ></AppointmentReferralModal>
 </template>
 
 <style>
@@ -117,6 +124,8 @@ import { PatientActions } from "@/store/enums/StorePatientEnums";
 import PatientAppointmentActions from "@/components/patients/PatientAppointmentActions.vue";
 import ViewReferralButton from "@/components/patients/patientAppointmentActions/ViewReferralButton.vue";
 import CollectingPersonButton from "@/components/patients/patientAppointmentActions/CollectingPersonButton.vue";
+import AppointmentReferralModal from "@/views/patients/modals/AppointmentReferralModal.vue";
+
 export default defineComponent({
   name: "patient-appointments",
   components: {
@@ -124,12 +133,14 @@ export default defineComponent({
     ViewReferralButton,
     PatientAppointmentActions,
     CollectingPersonButton,
+    AppointmentReferralModal,
   },
 
   setup() {
     const router = useRouter();
     const route = useRoute();
     const patient = computed(() => store.getters.selectedPatient);
+    const referralAppointment = ref(null);
 
     const userRole = computed(() => store.getters.userRole);
     const tableHeader = ref([
@@ -160,6 +171,10 @@ export default defineComponent({
       });
     };
 
+    const handleReferralClick = (item) => {
+      referralAppointment.value = item;
+    };
+
     watchEffect(() => {
       tableData.value = patient.value.appointments;
     });
@@ -183,7 +198,9 @@ export default defineComponent({
       tableHeader,
       tableData,
       userRole,
+      referralAppointment,
       handlePreAdmissionTest,
+      handleReferralClick,
       patient,
     };
   },
