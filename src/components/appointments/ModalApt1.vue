90e63eefe960395225428934472c3f80dc9d2e01
Shavinda Silva <shavinda34@gmail.com>
2022-12-15 16:06:55 +1030

    Implement new component for step 4 other info data

--- src/components/appointments/ModalApt1.vue
+++ src/components/appointments/ModalApt1.vue
@@ -81,7 +81,7 @@
                     stepperDescription="Doctor Address information and Appointment history"
                   />
                   <!--begin::Appointment Overview-->
-                  <AptOverview
+                  <apt-overview
                     :aptInfoData="aptInfoData"
                     :patientInfoData="patientInfoData"
                     :specialist="cur_specialist"
@@ -108,7 +108,7 @@
               <!--begin::Content-->
               <div class="flex-row-fluid py-lg-5 px-lg-15">
                 <!--begin::Step 1-->
-                <StepOne
+                <step-one
                   :loading="loading"
                   :modal-id="modalId"
                   :patient-info-data="patientInfoData"
@@ -122,7 +122,7 @@
                 <!--end::Step 1-->
 
                 <!--begin::Step 2-->
-                <StepTwo
+                <step-two
                   :loading="loading"
                   :modal-id="modalId"
                   :isNewPatient="isNewPatient"
@@ -135,7 +135,7 @@
                 <!--end::Step 2-->
 
                 <!--begin::Step 3 -->
-                <StepThree
+                <step-three
                   :loading="loading"
                   :modal-id="modalId"
                   @save="processSave"
@@ -147,242 +147,15 @@
                 <!--end::Step 3-->
 
                 <!--begin::Step 4 -->
-                <div data-kt-stepper-element="content">
-                  <div class="w-100">
-                    <el-form
-                      class="w-100"
-                      :model="otherInfoData"
-                      :rules="rules"
-                      ref="formRef_4"
-                      @submit.prevent=""
-                    >
-                      <div class="row scroll h-500px">
-                        <div v-if="apt_type == 'Procedure'" class="card-info">
-                          <div class="fs-3 fw-bold text-muted mb-6">
-                            Pre-Procedure Questions
-                          </div>
-                          <div class="row">
-                            <div class="col-sm-6">
-                              <!--begin::Input group-->
-                              <div class="fv-row mb-7">
-                                <!--begin::Input-->
-                                <el-form-item prop="anesthetic_questions">
-                                  <el-checkbox
-                                    class="w-100"
-                                    v-model="otherInfoData.anesthetic_questions"
-                                    label="Anesthetic Questions"
-                                    data-bs-toggle="collapse"
-                                    href="#toogle_ane_ques"
-                                  />
-                                </el-form-item>
-                                <!--end::Input-->
-                              </div>
-                              <!--end::Input group-->
-                            </div>
-                            <div
-                              class="col-sm-12 collapse"
-                              id="toogle_ane_ques"
-                            >
-                              <template
-                                v-for="(item, idx) in aneQuestions"
-                                :key="idx"
-                              >
-                                <div class="row mb-2">
-                                  <div class="col-10">{{ item.question }}</div>
-                                  <div class="col-2">
-                                    <el-switch
-                                      v-model="aneAnswers[idx]"
-                                      :active-value="true"
-                                      :inactive-value="false"
-                                      @change="handleAneQuestions"
-                                      style="
-                                        --el-switch-on-color: #13ce66;
-                                        --el-switch-off-color: #ff4949;
-                                      "
-                                      active-text="Y"
-                                      inactive-text="N"
-                                    />
-                                  </div>
-                                </div>
-                              </template>
-                            </div>
-                            <el-divider />
-                          </div>
-                        </div>
-                        <!--start::Doctor Address Information-->
-                        <div class="card-info">
-                          <div class="mb-6 d-flex justify-content-between">
-                            <span class="fs-3 fw-bold text-muted"
-                              >Doctor Address Information</span
-                            >
-                            <el-checkbox
-                              type="checkbox"
-                              v-model="otherInfoData.no_referral"
-                              label="No Doctor Address"
-                            />
-                          </div>
-                          <div class="row">
-                            <template v-if="otherInfoData.no_referral">
-                              <InputWrapper
-                                class="col-6"
-                                label="No Doctor Reason"
-                                prop="no_doctor_reason"
-                              >
-                                <el-input
-                                  type="text"
-                                  v-model="otherInfoData.no_referral_reason"
-                                  placeholder="Please Enter Reason"
-                                />
-                              </InputWrapper>
-                            </template>
-                            <template v-else>
-                              <InputWrapper
-                                class="col-6"
-                                label="Doctor Address Book"
-                                prop="doctor_address_book_id"
-                              >
-                                <el-autocomplete
-                                  class="w-100"
-                                  v-model="
-                                    otherInfoData.doctor_address_book_name
-                                  "
-                                  value-key="full_name"
-                                  :fetch-suggestions="searchDoctorAddressBook"
-                                  placeholder="Please input"
-                                  :trigger-on-focus="false"
-                                  @select="handleSelectDoctorAddressBook"
-                                >
-                                  <template #default="{ item }">
-                                    <div class="name">
-                                      {{ item.title }}
-                                      {{ item.first_name }}
-                                      {{ item.last_name }}
-                                    </div>
-                                    <div class="address">
-                                      {{ item.address }}
-                                    </div>
-                                  </template>
-                                </el-autocomplete>
-                              </InputWrapper>
-                              <InputWrapper
-                                class="col-6"
-                                label="Referral Duration"
-                                prop="referral_duration"
-                              >
-                                <el-select
-                                  class="w-100"
-                                  v-model="otherInfoData.referral_duration"
-                                  placeholder="Enter Referral Duration"
-                                >
-                                  <el-option value="0" label="Indefinite" />
-                                  <el-option value="3" label="3 Months" />
-                                  <el-option value="12" label="12 Months" />
-                                </el-select>
-                              </InputWrapper>
-
-                              <InputWrapper
-                                class="col-6"
-                                label="Referral Date"
-                                prop="referral_date"
-                              >
-                                <el-date-picker
-                                  editable
-                                  class="w-100"
-                                  format="DD-MM-YYYY"
-                                  v-model="otherInfoData.referral_date"
-                                />
-                              </InputWrapper>
-                            </template>
-                          </div>
-                        </div>
-                        <!--end::Referral Information-->
-                        <!--start::Appointment History-->
-                        <div class="card-info" v-if="patientStatus === 'exist'">
-                          <span class="fs-3 fw-bold text-muted"
-                            >Appointment History</span
-                          >
-
-                          <div style="color: grey">
-                            <span class="me-2"
-                              >Total Appointments:
-                              {{ patientAptData.appointment_count }}
-                            </span>
-                            <span class="me-2">
-                              <span class="me-2">/</span>Cancelled:
-                              {{ patientAptData.cancelled_appointment_count }}
-                            </span>
-                            <span class="me-2">
-                              <span class="me-2">/</span>Missed:
-                              {{ patientAptData.missed_appointment_count }}
-                            </span>
-                          </div>
-
-                          <AppointmentHistory
-                            :pastAppointments="patientAptData.pastAppointments"
-                            :futureAppointments="
-                              patientAptData.futureAppointments
-                            "
-                          />
-                        </div>
-                      </div>
-                      <!--end::Appointment History-->
-
-                      <div class="d-flex justify-content-between">
-                        <button
-                          type="button"
-                          class="btn btn-lg btn-light-primary me-3"
-                          data-kt-stepper-action="previous"
-                          @click="previousStep"
-                        >
-                          <span class="svg-icon svg-icon-4 me-1">
-                            <inline-svg
-                              src="media/icons/duotune/arrows/arr063.svg"
-                            />
-                          </span>
-                          Back
-                        </button>
-
-                        <button
-                          type="button"
-                          class="btn btn-lg btn-light-primary me-3"
-                          v-if="modalId == 'update'"
-                          @click="handleSave"
-                        >
-                          <span v-if="!loading" class="indicator-label">
-                            Save
-                          </span>
-                          <span v-if="loading" class="indicator-label">
-                            Please wait...
-                            <span
-                              class="spinner-border spinner-border-sm align-middle ms-2"
-                            ></span>
-                          </span>
-                        </button>
-                        <button
-                          type="button"
-                          class="btn btn-lg btn-primary align-self-end"
-                          @click="submit"
-                          v-else
-                        >
-                          <span v-if="!loading" class="indicator-label">
-                            Create Appointment
-                            <span class="svg-icon svg-icon-4 ms-1 me-0">
-                              <inline-svg
-                                src="media/icons/duotune/arrows/arr064.svg"
-                              />
-                            </span>
-                          </span>
-                          <span v-if="loading" class="indicator-label">
-                            Please wait...
-                            <span
-                              class="spinner-border spinner-border-sm align-middle ms-2"
-                            ></span>
-                          </span>
-                        </button>
-                      </div>
-                    </el-form>
-                  </div>
-                </div>
+                <step-four
+                  :apt_type="apt_type"
+                  :loading="loading"
+                  :modal-id="modalId"
+                  :other-data-e="otherInfoData"
+                  :patient-status="patientStatus"
+                  @save="processSave"
+                  @process="processStepFour"
+                />
                 <!--end::Step 4-->
               </div>
               <!--end::Content-->
@@ -429,6 +202,7 @@ import { useRouter } from "vue-router";
 import StepOne from "@/components/appointments/partials/ModalAptStep1";
 import StepTwo from "@/components/appointments/partials/ModalAptStep2";
 import StepThree from "@/components/appointments/partials/ModalAptStep3";
+import StepFour from "@/components/appointments/partials/ModalAptStep4";
 import AptOverview from "@/components/appointments/partials/AppointmentOverview";
 
 import { mask } from "vue-the-mask";
@@ -452,13 +226,12 @@ export default defineComponent({
     mask,
   },
   components: {
-    AppointmentHistory,
     StepperNavItem,
-    InputWrapper,
     AptOverview,
     StepOne,
     StepTwo,
     StepThree,
+    StepFour,
   },
 
   setup(props) {
@@ -1506,6 +1279,12 @@ export default defineComponent({
       _stepperObj.value.goNext();
     };
 
+    const processStepFour = () => {
+      loading.value = true;
+      props.modalId === "new" ? createApt() : updateApt();
+      resetCreateModal();
+    };
+
     return {
       chargeTypes,
       rules,
@@ -1591,6 +1370,7 @@ export default defineComponent({
       isNewPatient,
       processStepTwo,
       processStepThree,
+      processStepFour,
     };
   },
 });
